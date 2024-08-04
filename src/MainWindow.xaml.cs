9511b3a4f6a2b39fe38edd4c9a453ebfe4b24859
script-1024 <benny20050218@gmail.com>
2024-08-04 14:17:45 +0800

    initialize

---
+++ src/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.UI.Xaml;
+
+namespace HttpCrawler
+{
+    /// <summary>
+    /// 应用的主要窗口
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
