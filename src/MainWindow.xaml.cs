f5910c827e7fe4cf33d3207218e1450329233a24
script-1024 <benny20050218@gmail.com>
2024-08-04 19:03:48 +0800

    update

--- src/MainWindow.xaml.cs
+++ src/MainWindow.xaml.cs
@@ -1,15 +1,20 @@
-﻿using Microsoft.UI.Xaml;
+﻿using HttpCrawler.Core;
+using Microsoft.UI.Xaml;
 
 namespace HttpCrawler
 {
     /// <summary>
     /// 应用的主要窗口
     /// </summary>
-    public sealed partial class MainWindow : Window
+    public sealed partial class MainWindow : DesktopWindow
     {
         public MainWindow()
         {
             this.InitializeComponent();
+
+            // 设置窗口最小尺寸
+            this.MinHeight = 600;
+            this.MinWidth = 800;
         }
     }
 }
