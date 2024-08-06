27fd3ac39d92949c978af641b987036848cd931a
script-1024 <benny20050218@gmail.com>
2024-08-06 22:57:30 +0800

    feature: 实装 '网络请求' 页面

--- src/Pages/HttpRequestPage.xaml.cs
+++ src/Pages/HttpRequestPage.xaml.cs
@@ -13,5 +13,27 @@ public sealed partial class HttpRequestPage : Page
     public HttpRequestPage()
     {
         this.InitializeComponent();
+
+        RootPanel.Loaded += RootPanel_Loaded;
+    }
+
+    private void RootPanel_Loaded(object sender, RoutedEventArgs e)
+    {
+        // 操作 `ArgsPanel` 的属性必须在对象实例化后再进行，避免 NRE
+        ModeSwitcher.SelectionChanged += ModeSwitcher_SelectionChanged;
+    }
+
+    private void ModeSwitcher_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        ArgsPanel.Visibility = ModeSwitcher.SelectedIndex switch
+        {
+            1 => Visibility.Visible,
+            _ => Visibility.Collapsed
+        };
+    }
+
+    private void RequestButton_Click(object sender, RoutedEventArgs e)
+    {
+
     }
 }
