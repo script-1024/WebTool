b44cec4e389d8377e999f4f17193fa251e572616
script-1024 <benny20050218@gmail.com>
2024-08-07 00:46:19 +0800

    update

---
+++ src/Libraries/Helpers.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json;
+
+namespace HttpCrawler.Lib;
+
+public static class Helpers
+{
+    public static bool IsJsonStringValid(string json)
+    {
+        try { JsonDocument.Parse(json); return true; }
+        catch (JsonException) { return false; }
+    }
+}
